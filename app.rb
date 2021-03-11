9aa8bbb993584850d4079b36eb5f7780cdcdb2d2
Luis Castillo <raul@luiss-mbp.myfiosgateway.com>
2021-03-10 22:02:51 -0500

    commit tree 6e883f78f4c28a40e7a632531ce93be2154dc3eb

--- app.rb
+++ app.rb
@@ -3,4 +3,30 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get "/reversename/:name" do
+    @reverse_name = params[:name]
+    "#{@reverse_name.reverse!}"
+  end
+
+  get "/square/:number" do
+    @square = params[:number]
+    "#{@square.to_i * @square.to_i}"
+  end
+
+  get '/say/:number/:phrase' do
+    erb :say
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    erb :operation
+  end
 end
